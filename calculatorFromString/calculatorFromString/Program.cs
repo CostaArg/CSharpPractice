045fb0478481a3585d09cb45f2da1008b509899e
Konstantinos Argyropoulos <konstantinos.argyropoulos@Peoplecert.onmicrosoft.com>
2020-01-28 00:11:49 +0200

    added exercises

---
+++ calculatorFromString/calculatorFromString/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculatorFromString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your two numbers with either '*, /, -, +': ");
+            string input = Console.ReadLine();
+            string operand;
+
+            if (input.IndexOf("*", 0) != -1)
+            {
+                operand = input.Substring(input.IndexOf("*", 0), 1);
+            }
+            else if (input.IndexOf("/", 0) != -1)
+            {
+                operand = input.Substring(input.IndexOf("/", 0), 1);
+            }
+            else if (input.IndexOf("-", 0) != -1)
+            {
+                operand = input.Substring(input.IndexOf("-", 0), 1);
+            }
+            else
+            {
+                operand = input.Substring(input.IndexOf("+", 0), 1);
+            }
+
+            string[] number = new string[2];
+            number[0] = input.Substring(0, input.IndexOf(operand, 0));
+            number[1] = input.Substring(input.IndexOf(operand) + 1);
+
+            if (operand == "*")
+            {
+                Console.WriteLine(Convert.ToDouble(number[0]) * Convert.ToDouble(number[1]));
+
+            }
+            else if (operand == "/")
+            {
+                Console.WriteLine(Convert.ToDecimal(number[0]) / Convert.ToDecimal(number[1]));
+
+            }
+            else if (operand == "+")
+            {
+                Console.WriteLine(Convert.ToDouble(number[0]) + Convert.ToDouble(number[1]));
+
+            }
+            else
+            {
+                Console.WriteLine(Convert.ToDouble(number[0]) - Convert.ToDouble(number[1]));
+            }
+        }
+    }
+}
---
+++ codeWarsSumOddTriangle/codeWarsSumOddTriangle/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codeWarsSumOddTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long number = 5;
+            long result = Kata.rowSumOddNumbers(number);
+            Console.WriteLine(result);
+        }
+    }
+
+    public static class Kata
+    {
+        public static long rowSumOddNumbers(long n)
+        {
+            int counter = 1;
+            int counter2 = 1;
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                sum = 0;
+
+                for (int j = 0; j < counter2; j++)
+                {
+                    sum += counter;
+                    Console.Write(counter + " ");
+                    counter += 2;
+                }
+
+                Console.WriteLine();
+                counter2++;
+            }
+
+            Console.WriteLine("Sum is: " + sum);
+
+            return sum;
+        }
+    }
+}
