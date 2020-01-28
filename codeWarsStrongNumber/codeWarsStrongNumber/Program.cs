ba8af6321bcc089514fbdb0071882c8abac3b7c2
Konstantinos Argyropoulos <konstantinos.argyropoulos@Peoplecert.onmicrosoft.com>
2020-01-28 02:38:38 +0200

    added exercises

---
+++ codeWarsStrongNumber/codeWarsStrongNumber/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codeWarsStrongNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = Kata.StrongNumber(0);
+            Console.WriteLine(result);
+        }
+    }
+
+    class Kata
+    {
+        public static string StrongNumber(int number)
+        {
+            List<char> charList = new List<char>();
+            charList.AddRange(number.ToString());
+
+            List<int> toCalculateList = new List<int>();
+
+            foreach (var item in charList)
+            {
+                toCalculateList.Add(Convert.ToInt32(item.ToString()));
+            }
+
+            int sumFac = 0;
+            int saveNumber = number;
+
+            foreach (var item in toCalculateList)
+            {
+                int facNumber = Factorial(item);
+                Console.WriteLine(facNumber);
+                sumFac += facNumber;
+            }
+
+            if (sumFac == saveNumber)
+                return "STRONG!!!!";
+            else
+                return "Not Strong !!";
+        }
+
+        public static int Factorial(int number)
+        {
+            if (number == 0)
+                return 1;
+            else
+            {
+                int facNumber = 1;
+                while (number != 1)
+                {
+                    facNumber = facNumber * number;
+                    number = number - 1;
+                }
+                return facNumber;
+            }
+        }
+    }
+}
---
+++ codeWarsWhoseTurn/codeWarsWhoseTurn/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codeWarsWhoseTurn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string positions = "b1;g1;b8;g8";
+            bool turn = Kata.WhoseTurn(positions);
+            Console.WriteLine(turn);
+        }
+    }
+
+    public static class Kata
+    {
+        public static bool WhoseTurn(string positions)
+        {
+
+        }
+    }
+
+}
