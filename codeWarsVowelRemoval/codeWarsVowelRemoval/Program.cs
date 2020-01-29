79becf4cc6e9a7b98eb28a50addbb13b78a1bb77
Konstantinos Argyropoulos <konstantinos.argyropoulos@Peoplecert.onmicrosoft.com>
2020-01-29 13:48:15 +0200

    added exercises

---
+++ codeWarsVowelRemoval/codeWarsVowelRemoval/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codeWarsVowelRemoval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class Kata
+    {
+        public static string Shortcut(string input)
+        {
+            List<char> charList = new List<char>();
+
+            charList.AddRange(input);
+
+            List<char> charList2 = new List<char>();
+
+            for (int i = 0; i < charList.Count; i++)
+            {
+                if (charList[i] != 'a' && charList[i] != 'e' && charList[i] != 'i' && charList[i] != 'o' && charList[i] != 'u')
+                    charList2.Add(charList[i]);
+            }
+
+            input = null;
+
+            foreach (var item in charList2)
+            {
+                input += item;
+            }
+
+            return input;
+        }
+    }
+}
