42fff06294d9540bc867bb3943c75ee2bd31d8d6
Costas <konstadinos.argyropoulos@gmail.com>
2020-02-14 20:13:05 +0200

    added dataset lessons

---
+++ peiramataStringBuilder/peiramataStringBuilder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace peiramataStringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                sb.Append(i);
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
