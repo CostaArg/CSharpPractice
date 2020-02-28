7851f71e2e246762a3dea873aca52b29123b04e3
Konstantinos Argyropoulos <konstantinos.argyropoulos@Peoplecert.onmicrosoft.com>
2020-02-28 15:58:44 +0200

    added mvc exercise

---
+++ new/microsoftTutorial/microsoftTutorial/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace microsoftTutorial.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MvcMovie.Models.MovieDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(MvcMovie.Models.MovieDBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
